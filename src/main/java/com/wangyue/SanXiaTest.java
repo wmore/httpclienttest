f17bfd76afcd45d6ea5c4295e1ae1ba4063d4942
wmore <ywgjwangyue@sina.com>
2019-07-04 18:42:48 +0800

    xiuding

--- src/main/java/com/wangyue/SanXiaTest.java
+++
@@ -1,6 +0,0 @@
-package com.wangyue;
-
-public class SanXiaTest {
-
-//    public void
-}
--- src/main/java/com/wangyue/http/parser/BaseParser.java
+++ src/main/java/com/wangyue/http/parser/BaseParser.java
@@ -5,12 +5,15 @@ import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.springframework.web.util.HtmlUtils;
 
 import java.io.File;
 
 public class BaseParser {
 
-    public Element initDepartmentFromString(String xmlStr) {
+    public Element initDepartmentFromString(String xmlStr, String rootName) {
+        xmlStr = HtmlUtils.htmlUnescape(xmlStr);
+        System.out.println(xmlStr);
         // 创建saxReader对象
         SAXReader reader = new SAXReader();
         // 通过read方法读取一个文件 转换成Document对象
@@ -18,7 +21,8 @@ public class BaseParser {
         try {
             document = DocumentHelper.parseText(xmlStr);
             //获取根节点元素对象
-            Element node = document.getRootElement();
+            Element root = document.getRootElement();
+            Element node = root.element(rootName);
             //遍历所有的元素节点
             return node;
         } catch (DocumentException e) {
@@ -28,7 +32,7 @@ public class BaseParser {
         return null;
     }
 
-    public Element initDepartmentFromXmlFile(String xmlPath) {
+    public Element initDepartmentFromXmlFile(String xmlPath, String rootName) {
         // 创建saxReader对象
         SAXReader reader = new SAXReader();
         // 通过read方法读取一个文件 转换成Document对象
@@ -36,7 +40,8 @@ public class BaseParser {
         try {
             document = reader.read(new File(xmlPath));
             //获取根节点元素对象
-            Element node = document.getRootElement();
+            Element root = document.getRootElement();
+            Element node = root.element(rootName);
             //遍历所有的元素节点
             return node;
         } catch (DocumentException e) {
